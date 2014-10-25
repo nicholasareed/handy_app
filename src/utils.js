3c73a9e9c6b7ce5fc80d1e31a05d42c07555777c
Nicholas Reed <nicholas.a.reed@gmail.com>
2014-10-24 20:28:33 -0700

    .

--- src/utils.js
+++ src/utils.js
@@ -1382,7 +1382,7 @@ define(function (require) {
                     var viewUrl = 'inbox/' + payload.from_user_id;
                     if(App.Cache.currentPageViewPath == viewUrl ){
                         return;
-                    });
+                    }
                     Utils.Popover.Buttons({
                         title: 'New Message',
                         text: payload.text,
